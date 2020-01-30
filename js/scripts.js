75e3e7099ba02b4d9d6cde0b4654fba8b373e4bb
Stewart ilondanga <stilondanga@gmail.com>
2020-01-30 09:49:19 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,34 @@
 var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
+
+$.fn.serializeObject = function() {
+  var o = {};
+  var a = this.serializeArray();
+  $.each(a, function() {
+    if (o[this.name]) {
+      if (!o[this.name].push) {
+        o[this.name] = [o[this.name]];
+      }
+      o[this.name].push(this.value || '');
+    } else {
+      o[this.name] = this.value || '';
+    }
+  });
+  return o;
+};
+
+
+$('#order-form').submit(function(e) {
+  e.preventDefault();
+
+  var data = $(this).serializeObject();
+  var jsonDataString = JSON.stringify(data, null, 2);
+
+  $('#serialized').html('<code>' + jsonDataString + '</code>');
+});
